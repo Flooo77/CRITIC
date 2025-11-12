9eb70e2fee7688f0cbcc1fb6e0de66a7ae64c3be
Florian MASSOL <flo.massol77@gmail.com>
2025-11-13 00:17:22 +0100

    Improve require_session

--- backend/api/users/views.py
+++ backend/api/users/views.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import Session
 
 from api.database import get_db
 from .schemas import UserCreate, UserListing, UserStatusUpdate, LoginData
-from .controller import create_user, get_all_users, get_user_by_id, update_user_status, authenticate_user, delete_user_session, require_session, get_local_user
+from .controller import (create_user, get_all_users, get_user_by_id, 
+                         update_user_status, authenticate_user, delete_user_session, 
+                         require_session, get_local_user
+)
 
 router = APIRouter(
     prefix="/users",
@@ -11,7 +14,7 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.get('/', response_model=UserCreate)
+@router.post('/', response_model=UserCreate)
 def user_create(user: UserCreate, 
                 db: Session = Depends(get_db)
                 ):
@@ -24,18 +27,17 @@ def user_create(user: UserCreate,
 def users_get_all(request: Request, 
                   skip: int = 0, 
                   limit: int = 10, 
-                  db: Session = Depends(get_db)
+                  db: Session = Depends(get_db),
+                  _: None = Depends(require_session)
                   ):
-    require_session(request, db)
     users = get_all_users(db, skip=skip, limit=limit)
     return users
 
 @router.get('/{user_id}', response_model=UserListing)
-def user_get(request: Request,
-             user_id: int, 
-             db: Session = Depends(get_db)
+def user_get(user_id: int, 
+             db: Session = Depends(get_db),
+             _: None = Depends(require_session)
              ):
-    require_session(request, db)
     user = get_user_by_id(db, user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -44,7 +46,8 @@ def user_get(request: Request,
 @router.put('/{user_id}/status', response_model=UserStatusUpdate)
 def user_update_status(user_id: int, 
                        status_update: UserStatusUpdate, 
-                       db: Session = Depends(get_db)
+                       db: Session = Depends(get_db),
+                       _: None = Depends(require_session)
                        ):
     user = update_user_status(db, user_id, status_update.is_active)
     if user is None:
@@ -52,10 +55,9 @@ def user_update_status(user_id: int,
     return user
 
 @router.get('/local/')
-def user_get_local(request: Request,
-                    db: Session = Depends(get_db)
-                    ):
-    require_session(request, db)
+def user_get_local(db: Session = Depends(get_db),
+                   _: None = Depends(require_session)
+                   ):
     user = get_local_user(db)
     if user is None:
         raise HTTPException(status_code=404, detail="Local user not found")
@@ -65,16 +67,16 @@ def user_get_local(request: Request,
 def user_login(login_data: LoginData,
                db: Session = Depends(get_db)
                ):
-    auth = authenticate_user(db, login_data.username, login_data.password)
+    auth = authenticate_user(db, login_data)
     return auth
 
 @router.post('/logout/')
 def user_logout(request: Request,
-                db: Session = Depends(get_db)
+                db: Session = Depends(get_db),
+                _: None = Depends(require_session)
                 ):
-    require_session(request, db)
     try:
-        delete_user_session(request.cookies.get('session'), db)
+        delete_user_session(request, db)
     except:
         raise HTTPException(status_code=400, detail="Logout failed")
-    return {"detail": "Logout successful"}
+    return {"message": "Logout successful"}
